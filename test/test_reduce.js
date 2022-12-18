d8a378792440314455b27a358e85a7ba62818a3d
sqjoni <joona.j.nieminen@gmail.com>
2022-12-18 22:44:21 +0200

    implement more coverage

--- test/test_reduce.js
+++ test/test_reduce.js
@@ -2,7 +2,7 @@ import { assert, expect } from 'chai';
 import reduce from '../src/reduce.js';
 
 describe('#reduce', function(){
-    it('should reduce the given array correctly', function(){
+    it('should reduce the given object correctly', function(){
         const arr = reduce({ 'a': 1, 'b': 2, 'c': 1 }, (result, value, key) => {
                (result[value] || (result[value] = [])).push(key)
                return result
@@ -18,5 +18,14 @@ describe('#reduce', function(){
         const expected =  { 2:['a','c'], null:['b'] };
         expect(arr).to.deep.equal(expected);
     });
+    it('should work with different arrays', function() {
+        const arr = reduce([{'a':2, 'b':2}], (result, value, key) => {
+            (result[value] || (result[value] = [])).push(key)
+            return result
+         }, {})
+        const expected =  { "[object Object]": [0] };
+        expect(arr).to.deep.equal(expected);
+    });
+
 
 });
